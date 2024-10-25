d9a0e3e40562fad073389d0767739d80ea26222d
AbdulMonestel <abdulms@estudiantec.cr>
2024-10-25 10:09:59 -0600

    Update spi_master.py

--- codigo_comunicacion_serial/spi_master.py
+++ codigo_comunicacion_serial/spi_master.py
@@ -8,7 +8,7 @@ spi.max_speed_hz = 100000  # Configurar velocidad de SPI
 spi.mode = 0  # Configurar CPOL y CPHA (polarity = 0, phase = 0)
 
 def send_data(data):
-    spi.xfer2(data)  # Enviar datos al esclavo
+    spi.xfer2(data)
 
 def read_data(length):
     # Leer 'length' bytes del esclavo
@@ -21,15 +21,13 @@ try:
         message = b'\x01\x02\x03'
         #message = bytearray([0x01, 0x02, 0x03])
         print(f"Enviando: {message}")
-        send_data(message)  # Enviar datos al esclavo
+        send_data(message)
 
-        time.sleep(0.01)  # Pausa corta para dar tiempo al esclavo (ajusta si es necesario)
-
-        # Leer la respuesta del esclavo
-        response = read_data(3)  # Leer 3 bytes (ajusta según el tamaño esperado)
+        time.sleep(0.01)
+        response = read_data(3)
         print(f"Respuesta del esclavo: {response}")
 
-        time.sleep(1)  # Pausa de 1 segundo antes del siguiente ciclo
+        time.sleep(1)
 except KeyboardInterrupt:
     spi.close()
     print("SPI cerrado")
