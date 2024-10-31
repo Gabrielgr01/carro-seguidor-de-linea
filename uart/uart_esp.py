a71ac1c1175a658127157a74ef8f48dd3670b0ff
Gabriel González Rodríguez <gabrielgr01@hotmail.com>
2024-10-30 22:07:42 -0600

    Create uart_esp.py

---
+++ uart/uart_esp.py
@@ -0,0 +1,22 @@
+from machine import UART, Pin
+import time
+
+# Configuración UART en el ESP32
+uart = UART(1, baudrate=9600, tx=Pin(17), rx=Pin(16))  # TX=17, RX=16 en el ESP32
+# Led interno en el ESP32
+led = Pin(2, Pin.OUT)
+
+while True:
+    if uart.any():  # Si hay datos disponibles para leer
+        message = uart.readline().decode().strip()
+        print(f"Mensaje recibido de la Raspberry Pi: {message}")
+        
+        led.on()
+        
+        response = "Hola Pi"
+        uart.write(response)  # Enviar respuesta
+        print(f"Mensaje enviado: {response}")
+        time.sleep(1)
+      
+        led.off()
+    time.sleep(0.1)
