156160bd26b61f2567ffb126e7feb2efde6a077d
Gabriel González Rodríguez <gabrielgr01@hotmail.com>
2024-11-21 17:57:11 -0600

    Update main_esp.py
    Se agrego la versión más actual del seguimiento de linea y bifurcación

--- main_esp.py
+++ main_esp.py
@@ -1,4 +1,4 @@
-from machine import UART, Pin, time_pulse_us, PWM, ADC
+from machine import UART, Pin, time_pulse_us, PWM, ADC # type: ignore
 import time
 
 ######################
@@ -22,18 +22,18 @@ bifur_sel = 0
 
 dict_colores_funciones = {
     # Diccionario temporal. Luego tiene que definirse de los datos recibidos de la interfaz
-    "Seguir linea":"Negro",
-    "":"Blanco",
-    "Frenar":"Rojo",
-    "Retroceder":"Verde",
-    "Delta_v":"Azul"
+    "Negro":"Seguir Linea",
+    "Blanco":"Corregir Linea",
+    "Rojo":"Frenar",
+    "Verde":"Retroceder",
+    "Azul":"Delta_v"
 }
 
 
 ###########################
 # Inicialización de Pines #
 ###########################
-### Pines Inicio (interrupción)
+### Pines Inicio (interrupción) ###
 start_pin = Pin(25, Pin.IN)
 ### Pines Comunicacion UART ###
 uart = UART(1, baudrate=9600, tx=Pin(17), rx=Pin(16))  # Tx en pin 17 y Rx en pin 16
@@ -47,23 +47,28 @@ OUT = Pin(13, Pin.IN)
 S0.value(0)  # Alta frecuencia
 S1.value(1)
 ### Pines Motores ###
-in1 = Pin(19, Pin.OUT)  # Pin para IN1 del Motor A (L298)
-in2 = Pin(18, Pin.OUT)  # Pin para IN2 del Motor A (L298)
+in1 = Pin(19, Pin.OUT)  # Pin para IN1 del Motor A (usando el L298)
+in2 = Pin(18, Pin.OUT)  # Pin para IN2 del Motor A (usando el L298)
 ena = PWM(Pin(5), freq=2000)  # Pin para ENA del Motor A (PWM)
 in3 = Pin(22, Pin.OUT)  # Pin para IN3 del Motor B
 in4 = Pin(23, Pin.OUT)  # Pin para IN4 del Motor B
 enb = PWM(Pin(21), freq=2000)  # Pin para ENB del Motor B (PWM)
 ### Pines Sensores infrarrojos ANALÓGICOS ###
 IR_derecha = ADC(Pin(32))  
-IR_izquierda = ADC(Pin(35))
+IR_izquierda = ADC(Pin(35)) 
 IR_derecha.atten(ADC.ATTN_11DB)   # Rango completo de 0 a 3.3V
 IR_izquierda.atten(ADC.ATTN_11DB) # Rango completo de 0 a 3.3V
+### Variables de control ###
+threshold = 900  # Menos de 900 es blanco, más de 900 es negro
+velocidad_base = 550  # Velocidad base de los motores
+velocidad_inicial = 550 # Velocidad inicial para el arranque
 
 #############
 # Funciones #
 #############
 
 ### Función para controlar la dirección y velocidad del Motor A ###
+# El motor A es el izquierdo
 def motor_a(direccion, velocidad):
     if direccion == "adelante":
         in1.off()
@@ -78,6 +83,7 @@ def motor_a(direccion, velocidad):
     ena.duty(velocidad)
 
 ### Función para controlar la dirección y velocidad del Motor B ###
+# El motor B es el derecho
 def motor_b(direccion, velocidad):
     if direccion == "adelante":
         in3.on()
@@ -97,32 +103,26 @@ def detener_motores():
     motor_b("detener", 0)
 
 ### Función para seguir la lógica de movimiento ###
-def seguir_linea():
+def seguir_linea(dir):
     sensorR = IR_derecha.read()
     sensorL = IR_izquierda.read()
-    viraje = ""
 
-    print(f"Sensor Derecho: {sensorR}, Izquierdo: {sensorL}")
+    #print(f"Sensor Derecho: {sensorR}, Izquierdo: {sensorL}")
 
     # Si ambos sensores están en blanco, avanzar en línea recta
     if sensorL < threshold and sensorR < threshold:
-        print("Línea centrada. Avanzando recto.")
-        motor_a("adelante", velocidad_base)
-        motor_b("adelante", velocidad_base)
+        motor_a(dir , velocidad_base)
+        motor_b(dir , velocidad_base)
     
-    # Si el sensor izquierdo detecta negro y el derecho blanco, corregir hacia la izquierda
+    # Si el sensor izquierdo detecta negro, corregir hacia la izquierda
     elif (sensorL > threshold) and (sensorR < threshold):
-        print("Corrigiendo hacia la izquierda.")
-        motor_a("adelante", 0)  # Detener motor izquierdo
-        motor_b("adelante", velocidad_base)
-        time.sleep(0.1)  # Pausa para corrección
+        motor_a(dir , 0)  # Detener motor izquierdo
+        motor_b(dir , velocidad_base)
     
-    # Si el sensor derecho detecta negro y el izquierdo blanco, corregir hacia la derecha
+    # Si el sensor derecho detecta negro, corregir hacia la derecha
     elif (sensorR > threshold) and (sensorL < threshold):
-        print("Corrigiendo hacia la derecha.")
-        motor_a("adelante", velocidad_base)
-        motor_b("adelante", 0)  # Detener motor derecho
-        time.sleep(0.1)  # Pausa para corrección
+        motor_a(dir , velocidad_base)
+        motor_b(dir , 0)  # Detener motor derecho
 
     # Manejo de la bifurcación
     # Si ambos sensores detectan negro se gira en una dirección seleccionada 
@@ -130,19 +130,21 @@ def seguir_linea():
     elif (sensorR > threshold) and (sensorL > threshold):
         
         if (bifur_sel == 0):
-            viraje = "derecha"
-            motor_a("adelante", velocidad_base)
-            motor_b("adelante", 0) 
+            motor_a(dir , velocidad_base)
+            motor_b(dir , 0) 
 
-        elif (bifur_sel ==1):
-            viraje = "izquierda"
-            motor_a("adelante", 0)
-            motor_b("adelante", velocidad_base)
-        
-        print(f"Bifurcación detectada, corrigiendo hacia la {viraje}")
+        elif (bifur_sel == 1):
+            motor_a(dir , 0)
+            motor_b(dir , velocidad_base)
 
-def retroceder():
-    print("Ejecutando Retroceder")
+def arranque():
+    motor_a("adelante", velocidad_inicial)
+    motor_b("adelante", velocidad_inicial)
+    time.sleep(0.2)
+
+def retroceso():
+    seguir_linea("atras")
+    time.sleep(3)
 
 def cambiar_velocidad():
     print("Ejecutando Cambio Velocidad")
@@ -267,11 +269,18 @@ while True:
     if start_flag == 1:
         led.on()
         print("-I- Carrito iniciado")
+        
+        arranque("adelante")
+        time.sleep(0.08)  # Delay entre las actualizaciones
+        detener_motores()
+        time.sleep(0.1) # Delay entre las actualizaciones
 
         #color = detect_color()
         #print(f"Color detectado: {color}")
         #ejecutar_maniobra(color)
     else:
+        detener_motores()
+
         led.off()
 
     time.sleep(0.5) # Sleep necesario para que le de tiempo al buffer de datos de recibir todos los bits     
